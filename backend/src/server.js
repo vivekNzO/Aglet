c1256892cf4caf9eeb00a1a6c2dff5ab8d1d7f04
vivekNzO <vivek@netzoptimize.com>
2025-12-27 11:40:01 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import path from "path";
+import { ENV } from './config/env.js';
 
 const app = express();
+
+const __dirname = path.resolve()
 
 app.get("/api/health",(req,res)=>{
     res.status(200).json({message:"Success"})
 })
 
-app.listen(3000,()=>{
+// make app ready for deployment
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"))
+    })
+}
+
+
+app.listen(ENV.PORT,()=>{
     console.log("Server is up and running")
 })
